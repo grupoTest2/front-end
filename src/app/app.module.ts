2698aeff5daa75494203e5ab59fe552ee3c96a49
kevinneyder <kevinneyderbutronm@gmail.com>
2020-05-22 21:24:56 -0400

    Validaciones de formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
@@ -22,6 +23,7 @@ import { FormConvocatoriaDocenciaComponent } from './components/form-convocatori
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
--- src/app/components/form-convocatoria-docencia/form-convocatoria-docencia.component.ts
+++ src/app/components/form-convocatoria-docencia/form-convocatoria-docencia.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PhpServeService } from 'src/app/servicios/form-convocatoria-docencia/php-serve.service';
+
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 // import $ from "jquery";
 declare var swal: any;
 declare var $: any;
@@ -9,60 +12,89 @@ declare var $: any;
   styleUrls: ['./form-convocatoria-docencia.component.css']
 })
 export class FormConvocatoriaDocenciaComponent implements OnInit {
-  idDepartamento=1;
-  seleccionMateria:SeleccionMateria;
-  materia={
-    nombreMat:null,
-    idMat:null,
-    cantidadAux: null,
-    hrsMes:null
-  }
-  materiasSeleccionadas:Object[]= new Array();
-  listaMaterias:Object[]=new Array();
-  listaMateriasDisponibles:String[];
 
-  constructor(private apiPHP: PhpServeService) { }
-  
+  formRequerimientos: FormGroup;
+
+  idDepartamento = 1;
+  seleccionMateria: SeleccionMateria;
+  materia = {
+    nombreMat: null,
+    idMat: null,
+    cantidadAux: null,
+    hrsMes: null
+  }
+  materiasSeleccionadas: Object[] = new Array();
+  listaMaterias: Object[] = new Array();
+  listaMateriasDisponibles: String[];
+
+  constructor(private formBuilder: FormBuilder, private apiPHP: PhpServeService) {
+
+  }
+
   ngOnInit(): void {
     this.getNombreMaterias();
   }
-  getNombreMaterias(){
+
+  // formularios con validaciones
+  private buildForm() {
+    this.formRequerimientos = this.formBuilder.group({
+      items: ['',  [Validators.required]],
+      cantidadAux: ['', [Validators.required]],
+      horasMes: ['', [Validators.required]],
+      materia: ['', [Validators.required]],
+    });
+
+    this.formRequerimientos.valueChanges
+      .subscribe(value => {
+        console.log(value);
+      });
+  }
+  save(event: Event){
+      event.preventDefault();
+      const value = this.formRequerimientos.value;
+      console.log(value);
+  }
+
+
+
+  getNombreMaterias() {
     this.apiPHP.getNombreMaterias(this.idDepartamento).subscribe(
-      result =>{ 
-        for(let i in result){
+      result => {
+        for (let i in result) {
           this.listaMaterias.push(result[i]);
         }
-      this.seleccionMateria=new SeleccionMateria(this.listaMaterias);
-      this.listaMateriasDisponibles=this.seleccionMateria.getListaMateriasDisponibles();
+        this.seleccionMateria = new SeleccionMateria(this.listaMaterias);
+        this.listaMateriasDisponibles = this.seleccionMateria.getListaMateriasDisponibles();
       }
     );
   }
-  guardarMateria(){
+  guardarMateria() {
     console.log(this.materia.cantidadAux);
     console.log(this.materia.hrsMes);
     console.log(this.materia.nombreMat);
-    let objAux= JSON.parse(JSON.stringify(this.materia));
+    let objAux = JSON.parse(JSON.stringify(this.materia));
     this.seleccionMateria.agregarMateriaSeleccionada(objAux);
-    this.materiasSeleccionadas=this.seleccionMateria.getMateriasSeleccionadas();
+    this.materiasSeleccionadas = this.seleccionMateria.getMateriasSeleccionadas();
     //console.log(this.materiasSeleccionadas); 
+    console.log("funciona el boton");
   }
-  agregarMateria(){
-    
-    this.materia.cantidadAux=null;
-    this.materia.hrsMes=null;
-    this.listaMateriasDisponibles=this.seleccionMateria.getListaMateriasDisponibles();
+  agregarMateria() {
+
+    this.materia.cantidadAux = null;
+    this.materia.hrsMes = null;
+    this.listaMateriasDisponibles = this.seleccionMateria.getListaMateriasDisponibles();
     console.log(this.listaMateriasDisponibles);
-    
+
   }
-  agregarMateriaBD(){
+  agregarMateriaBD() {
     this.apiPHP.agregarMateria(this.seleccionMateria.getMateriasSeleccionadas()).subscribe(
       datos => {
-         alert(datos['mensaje']);
-       }
-       );
-    }
-    
-  alertEliminar(){
+        alert(datos['mensaje']);
+      }
+    );
+  }
+
+  alertEliminar() {
     swal.fire({
       title: 'Eliminar',
       text: "¿Desea eliminar el campo seleccionado?",
@@ -79,7 +111,7 @@ export class FormConvocatoriaDocenciaComponent implements OnInit {
           'El campo fue eliminado.',
           'success'
         )
-      }else{
+      } else {
         swal.fire(
           'Cancelado!',
           'El campo no fue eliminado.',
@@ -89,49 +121,55 @@ export class FormConvocatoriaDocenciaComponent implements OnInit {
     })
   }
 
-}
-class SeleccionMateria{
-  //mis atributos
-  private materiasSeleccionadas:Object[];
-  private listaMaterias:Object[];
-  private listaMateriasDisponibles:String[];
 
-  constructor(listaMat){
-    this.materiasSeleccionadas=new Array();
-    this.listaMaterias=listaMat;
+
+
+
+
+  
+}
+class SeleccionMateria {
+  //mis atributos
+  private materiasSeleccionadas: Object[];
+  private listaMaterias: Object[];
+  private listaMateriasDisponibles: String[];
+
+  constructor(listaMat) {
+    this.materiasSeleccionadas = new Array();
+    this.listaMaterias = listaMat;
     //console.log(this.listaMaterias);
     this.actualizarListaMatDisponibles();
   }
 
-  public actualizarListaMatDisponibles(){
-    this.listaMateriasDisponibles=new Array();
-    for(let i in this.listaMaterias){
-      let mat:any=this.listaMaterias[i];
-      if(!mat.seleccionado){
+  public actualizarListaMatDisponibles() {
+    this.listaMateriasDisponibles = new Array();
+    for (let i in this.listaMaterias) {
+      let mat: any = this.listaMaterias[i];
+      if (!mat.seleccionado) {
         this.listaMateriasDisponibles.push(mat.nombreMat);
-        
+
       }
     }
     //console.log(this.listaMateriasDisponibles);
   }
   //devuelve el id de la materia que se esta buscando
-  getIdMateria(nombMat){
-    let res=-1;
-    for(let i in this.listaMaterias){
-      let mat:any=this.listaMaterias[i];
-      if(mat.nombreMat==nombMat){
-        res= mat.idMat;
+  getIdMateria(nombMat) {
+    let res = -1;
+    for (let i in this.listaMaterias) {
+      let mat: any = this.listaMaterias[i];
+      if (mat.nombreMat == nombMat) {
+        res = mat.idMat;
         break;
       }
     }
     return res;
   }
 
-  public agregarMateriaSeleccionada(materia){
-    if(materia.nombreMat!=null && materia.cantidadAux!=null && materia.hrsMes!=null){
-      let idMateria=this.getIdMateria(materia.nombreMat);
-      if(idMateria!=-1){
-        materia.idMat=idMateria;   
+  public agregarMateriaSeleccionada(materia) {
+    if (materia.nombreMat != null && materia.cantidadAux != null && materia.hrsMes != null) {
+      let idMateria = this.getIdMateria(materia.nombreMat);
+      if (idMateria != -1) {
+        materia.idMat = idMateria;
         this.deshabilitarSeleccion(materia.nombreMat);
         this.materiasSeleccionadas.push(materia);
         console.log(JSON.stringify(this.materiasSeleccionadas));
@@ -139,24 +177,24 @@ class SeleccionMateria{
     }
   }
 
-  public deshabilitarSeleccion(nombreMatForm){
-    for(let i in this.listaMaterias){
-      let mat:any=this.listaMaterias[i];
-      if(mat.nombreMat==nombreMatForm){
-        mat.seleccionado=true;
+  public deshabilitarSeleccion(nombreMatForm) {
+    for (let i in this.listaMaterias) {
+      let mat: any = this.listaMaterias[i];
+      if (mat.nombreMat == nombreMatForm) {
+        mat.seleccionado = true;
       }
     }
     this.actualizarListaMatDisponibles();
   }
-  public existeMateriasSeleccionas(){
-    return this.materiasSeleccionadas.length>0;
+  public existeMateriasSeleccionas() {
+    return this.materiasSeleccionadas.length > 0;
   }
-  
-  public getListaMateriasDisponibles(){
+
+  public getListaMateriasDisponibles() {
     return this.listaMateriasDisponibles;
   }
 
-  public getMateriasSeleccionadas(){
+  public getMateriasSeleccionadas() {
     return this.materiasSeleccionadas;
   }
 }
