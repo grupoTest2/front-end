6fc4081f5db57590fff32b1841d56825b2b292fe
josueJimenez97 <josuejnumss97@gmail.com>
2020-05-23 22:12:23 -0400

    agregando modals

--- src/app/models/convocatoria-docente/requisito.ts
+++ src/app/models/convocatoria-docente/requisito.ts
@@ -1,2 +1,21 @@
 export class Requisito {
+    
+  private listaRequisitos:Object[];
+  private listaRequisitosSeleccionados:String[];
+  public constructor(){
+    this.listaRequisitosSeleccionados=new Array();
+    for(let i=1;i<5;i++){
+      let mensaje="este es un requisito"+i;
+      this.listaRequisitosSeleccionados.push(mensaje);
+    }
+  }
+  public getInciso(num){
+    //console.log("veamos............");
+    let inciso=String.fromCharCode(97+num)+")";
+    //console.log(inciso);
+    return inciso;
+  }
+  public getListaRequisitosSeleccionados():String[]{
+    return this.listaRequisitosSeleccionados;
+  }
 }
