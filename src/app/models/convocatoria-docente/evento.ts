e7408f59746308aa63d7685aa427e81631c3422d
josueJimenez97 <josuejnumss97@gmail.com>
2020-06-01 18:02:06 -0400

    componente fecha enlazado con su seleccion-evento, con sus validaciones

--- src/app/models/convocatoria-docente/evento.ts
+++ src/app/models/convocatoria-docente/evento.ts
@@ -5,13 +5,16 @@ export class Evento{
     private fechaFin:Date;
     private horaIni:String;
     private horaFin:String;
-
-    public constructor(nombre,fechaIni,fechaFin,horaIni=" ",horaFin=" "){
+    private fechaIniString:String;
+    private fechaFinString:String;
+    public constructor(nombre,fecha,horaFin="",horaIni=""){
         this.nombre=nombre;
-        this.fechaIni=fechaIni;
-        this.fechaFin=fechaFin;
-        this.horaIni=horaIni;
+        this.construirFecha(fecha);
         this.horaFin=horaFin;
+    }
+    private construirFecha(fecha:String){
+        let aux=fecha.split("/",3);
+        this.fechaFin=new Date(aux[2]+"-"+aux[1]+"-"+aux[0]);
     }
     public getNombre(){
         return this.nombre;
@@ -29,13 +32,21 @@ export class Evento{
         return this.horaFin;
     }
     public getFecha():String{
-        return this.fechaFin.getDay()+"/"+this.fechaFin.getMonth()+"/"+this.fechaFin.getUTCFullYear();
+        return this.fechaFin.toLocaleString().split(" ")[0];
     }
     public setFechaIni(nFecha){
         this.fechaIni=nFecha;
     }
     public setFechaFin(nFecha){
         this.fechaFin=nFecha;
+    }
+    public convertirseString(){
+        let fechaF=this.fechaFin.toLocaleString().split(" ")[0];
+        let fechaI=this.fechaIni.toLocaleString().split(" ")[0];
+        let aux=fechaF.split("/",3);
+        this.fechaFinString=aux[2]+"-"+aux[1]+"-"+aux[0];
+        aux=fechaI.split("/",3);
+        this.fechaFinString=aux[2]+"-"+aux[1]+"-"+aux[0];
     }
     public setIdLanzConv(nIdLanzConv){
         this.idLanzConv=nIdLanzConv;
