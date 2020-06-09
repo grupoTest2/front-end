4804ebeaacb662cbd3eceabfb65085ffa9a4c05c
josueJimenez97 <josuejnumss97@gmail.com>
2020-06-09 01:51:38 -0400

    funciones de interaccion con la BD agregada

--- src/app/pages/crearConvocatoria/crearConvocatoria.component.ts
+++ src/app/pages/crearConvocatoria/crearConvocatoria.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { TipoConvocatoria } from '../../models/clases/crear-convocatoria/tipo-convocatoria';
 import { Router } from '@angular/router';
+import { PhpServeConvocatoria } from 'src/app/servicios/form-convocatoria/php-serve.service';
+import { Convocatoria } from 'src/app/models/clases/crear-convocatoria/convocatoria';
 
 
 declare var tata: any;
@@ -16,12 +18,13 @@ export class crearConvocatoriaComponent implements OnInit {
 
   //formulario validaciones
   formCrearConv: FormGroup;
-  tipo1: TipoConvocatoria = new TipoConvocatoria(1, 'docencia');
-  tipo2: TipoConvocatoria = new TipoConvocatoria(2, 'laboratorio');
-  listaTiposConvocatoria: TipoConvocatoria[] = [this.tipo1, this.tipo2];
-
-  constructor(private formBuilder: FormBuilder, private router: Router) {
+  //tipo1: TipoConvocatoria = new TipoConvocatoria(1, 'docencia');
+  //tipo2: TipoConvocatoria = new TipoConvocatoria(2, 'laboratorio');
+  //listaTiposConvocatoria: TipoConvocatoria[] = [this.tipo1, this.tipo2];
+  listaTiposConvocatoria: TipoConvocatoria[]=[];
+  constructor(private formBuilder: FormBuilder,  private apiPHP: PhpServeConvocatoria,private router: Router) {
     this.buildForm();
+    this.getTipoConvocatoriaBD();
   }
 
   ngOnInit(): void {
@@ -91,5 +94,44 @@ export class crearConvocatoriaComponent implements OnInit {
   ruta(){
     var id = $('#tipoConvocatoria').val();
     this.router.navigate(['crearConvocatoria/tipo', id]);
+  }
+  /*---------------------------------interaccion con la BD-------------------------------*/
+  getTipoConvocatoriaBD(){
+    let idDep=1;
+    let listaTipos:any[]=new Array();
+    this.apiPHP.getTipoConvocatoria(idDep).subscribe(
+      result => {
+        for (let i in result) {
+          listaTipos.push(result[i]);
+        }
+        //console.log(listaTipos);
+        let tipo: TipoConvocatoria;
+        for(let i in listaTipos){
+          tipo= new TipoConvocatoria(listaTipos[i].idTipoConv,listaTipos[i].nombre);
+          //console.log(tipo);
+          this.listaTiposConvocatoria.push(tipo);
+        }
+      }
+    );
+    
+  }
+
+  crearConvocatoriaBD(){
+    let res:boolean=false;
+    let objAux:Convocatoria;
+    this.apiPHP.crearConvocatoria(objAux).subscribe(
+      respuesta => {
+        if (respuesta['resultado'] == 'correcto') {
+          //se crea correctamente la convocatoria
+          if(respuesta['mensaje']!=-1){
+            objAux.setIdConv(respuesta['mensaje']);
+            res=true;
+          }
+        } else {
+          //no se pudo agregar
+        }
+      }
+    );
+    return res;
   }
 }
