a64fe3f35c45958343e5c5602c9fe7262a17df86
josueJimenez97 <josuejnumss97@gmail.com>
2020-06-15 22:27:15 -0400

    ultimos cambios

--- src/app/components/datos-rotulo/datos-rotulo.component.ts
+++ src/app/components/datos-rotulo/datos-rotulo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SeleccionTipoDatoRotulo } from 'src/app/models/convocatoria/seleccion-tipo-dato-rotulo';
+import { PhpServeConvocatoria } from 'src/app/servicios/form-convocatoria/php-serve.service';
 
 @Component({
   selector: 'app-datos-rotulo',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatosRotuloComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private apiPHP: PhpServeConvocatoria) { 
+    this.getTipoDatosRotulo();
+  }
   ngOnInit(): void {
   }
 
+  getTipoDatosRotulo(){
+    let seleccion:SeleccionTipoDatoRotulo; 
+    let listaTipos: object[] = new Array();
+    this.apiPHP.getTipoDatosRotulo().subscribe(
+      resultado => {
+        for (let i in resultado) {
+          listaTipos.push(resultado[i]);
+        }
+        seleccion=new SeleccionTipoDatoRotulo(listaTipos);
+        console.log(JSON.stringify(seleccion.getListaTiposDatosRotulo()));
+      }
+    );
+  }
 }
