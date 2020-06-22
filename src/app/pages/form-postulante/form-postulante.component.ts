c6cf3b461d7854d9c4c5d13a83fee6482254d516
jhonn William <jhonnwcl96@gmail.com>
2020-06-22 17:00:02 -0400

    guardanado cambios

--- src/app/pages/form-postulante/form-postulante.component.ts
+++ src/app/pages/form-postulante/form-postulante.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { from } from 'rxjs';
+import {TipoDatoRotulo } from 'src/app/models/clases/convocatoria/tipo-dato-rotulo';
+
 
 @Component({
   selector: 'app-form-postulante',
@@ -8,9 +10,21 @@ import { from } from 'rxjs';
 })
 export class FormPostulanteComponent implements OnInit {
 
+  listaDatosRotulo:TipoDatoRotulo[]=[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.cargarDatos();
+
+  }
+  cargarDatos(){
+    this.listaDatosRotulo.push(new TipoDatoRotulo("nombre",true,true,"text"));
+    this.listaDatosRotulo.push(new TipoDatoRotulo("correo",true,true,"email"));
+    this.listaDatosRotulo.push(new TipoDatoRotulo("telefono",true,true,"number"));
+    this.listaDatosRotulo.push(new TipoDatoRotulo("edad",true,true,"number"));
+    this.listaDatosRotulo.push(new TipoDatoRotulo("correo2",true,true,"email"));
+    this.listaDatosRotulo.push(new TipoDatoRotulo("codigosis",true,true,"number"));
   }
 
 }
--- src/app/pages/form-registro/form-registro.component.ts
+++ src/app/pages/form-registro/form-registro.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import {TipoDatoRotulo } from 'src/app/models/clases/convocatoria/tipo-dato-rotulo';
 
 @Component({
   selector: 'app-form-registro',
@@ -9,18 +8,9 @@ import {TipoDatoRotulo } from 'src/app/models/clases/convocatoria/tipo-dato-rotu
 })
 export class FormRegistroComponent implements OnInit {
 
-listaDatosRotulo:TipoDatoRotulo[]=[];
   constructor() { }
 
   ngOnInit(): void {
-    this.cargarDatos();
   }
-  cargarDatos(){
-    this.listaDatosRotulo.push(new TipoDatoRotulo("nombre",true,true,"text"));
-    this.listaDatosRotulo.push(new TipoDatoRotulo("correo",true,true,"email"));
-    this.listaDatosRotulo.push(new TipoDatoRotulo("telefono",true,true,"number"));
-    this.listaDatosRotulo.push(new TipoDatoRotulo("edad",true,true,"number"));
-    this.listaDatosRotulo.push(new TipoDatoRotulo("correo2",true,true,"email"));
-    this.listaDatosRotulo.push(new TipoDatoRotulo("codigosis",true,true,"number"));
-  }
+ 
 }
