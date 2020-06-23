ce7f7e0bc9b09b9d855e76e1a7cae5177ec44aff
jhonn William <jhonnwcl96@gmail.com>
2020-06-23 00:32:01 -0400

    push

--- src/app/pages/form-postulante/form-postulante.component.ts
+++ src/app/pages/form-postulante/form-postulante.component.ts
@@ -15,10 +15,12 @@ export class FormPostulanteComponent implements OnInit {
   listaDatosRotulo: TipoDatoRotulo[] = [];
   bandera = true;
 
-  msjErrorNumericoVacio = "campo numerico vacio"
+  msjErrorNumericoVacio = "datos incorrectos"
+  msjErrorNumericoIncorrecto="campo numerico contiene caracteres incorrectos";
+  msjErrorNumericoCorto = "campo numerico corto"
   msjTextoVacio = "campo de texto vacio"
   msjTextocorto = "texto demasiado corto"
-  msjErrorNumericoCorto = "campo numerico corto"
+  msjTextoIncorrecto = "texto incorrecto"
   msjErrorEmailVacio = "campo de correo vacio"
   msjErrorEmailCorto = "campo de correo muy corto"
   msjErrorEmailIncorrecto = "correo incorrecto";
@@ -70,26 +72,38 @@ export class FormPostulanteComponent implements OnInit {
         let value = $("#" + id).val();
         var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
 
-        //inputs tipo text
+        //inputs tipo number
         if (inputTipe == "number") {
-          if (value.length == 0) {
+           value = $("#" + id).val();
+          if (value.length ===0) {
             $("#" + id).addClass("is-invalid");
             $("#" + id + "11").css('display', 'block');
+            $("#" + id + "12").css('display', 'none');
+            $("#" + id + "13").css('display', 'none');
           }
           if (value.length < 1 && value.length > 0) {
             $("#" + id).addClass("is-invalid");
             $("#" + id + "11").css('display', 'none');
             $("#" + id + "12").css('display', 'block');
+            $("#" + id + "13").css('display', 'none');
           }
           if (value.length >= 1) {
             $("#" + id).removeClass("is-invalid");
             $("#" + id).addClass("is-valid");
             $("#" + id + "11").css('display', 'none');
             $("#" + id + "12").css('display', 'none');
+            $("#" + id + "13").css('display', 'none');
+          }
+          if(value.includes('.') || value.includes('e')){
+            $("#" + id).addClass("is-invalid");
+            $("#" + id + "11").css('display', 'none');
+            $("#" + id + "12").css('display', 'none');
+            $("#" + id + "13").css('display', 'block');
           }
         }
         //input tipo texto
         if (inputTipe == "text") {
+          value = $("#" + id).val();
           if (value.length == 0) {
             $("#" + id).addClass("is-invalid");
             $("#" + id + "21").css('display', 'block');
@@ -106,10 +120,17 @@ export class FormPostulanteComponent implements OnInit {
             $("#" + id + "21").css('display', 'none');
             $("#" + id + "22").css('display', 'none');
           }
+          if(value.includes('.')){
+            $("#" + id).addClass("is-invalid");
+            $("#" + id + "21").css('display', 'none');
+            $("#" + id + "22").css('display', 'none');
+            $("#" + id + "23").css('display', 'block');
+          }
         }
 
         //input tipo gmail
         if (inputTipe == "email") {
+        value = $("#" + id).val();
           if (value.length == 0) {
             $("#" + id).removeClass("is-valid");
             $("#" + id).addClass("is-invalid");
@@ -159,6 +180,7 @@ export class FormPostulanteComponent implements OnInit {
             $("#" + id).addClass("is-valid");
             $("#" + id + "11").css('display', 'none');
             $("#" + id + "12").css('display', 'none');
+            $("#" + id + "13").css('display', 'none');
           }
         }
         if (inputTipe == "email") {
