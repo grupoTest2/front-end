4e59ca40806300d22271ec6f7acfa05cce302811
jhonn William <jhonnwcl96@gmail.com>
2020-07-03 15:00:00 -0400

    modificando

---
+++ src/app/pages/asingacion-area-conocimiento/asingacion-area-conocimiento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asingacion-area-conocimiento',
+  templateUrl: './asingacion-area-conocimiento.component.html',
+  styleUrls: ['./asingacion-area-conocimiento.component.css']
+})
+export class AsingacionAreaConocimientoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
