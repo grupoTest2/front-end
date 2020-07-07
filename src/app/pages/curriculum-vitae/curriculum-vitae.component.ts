a6ec01aaa94a05d31c53282fc58239f0f831fecd
kevinneyder <kevinneyderbutronm@gmail.com>
2020-07-07 16:45:29 -0400

    validaciones

--- src/app/pages/curriculum-vitae/curriculum-vitae.component.ts
+++ src/app/pages/curriculum-vitae/curriculum-vitae.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, NgForm } from '@angular/forms';
+declare var tata: any;
+declare var $: any;
+
+export interface FormObject {
+  email: string;
+  singleSelect: string;
+  multiSelect: string;
+  textarea: string;
+  radio: string;
+  checkbox: any;
+}
 
 @Component({
   selector: 'app-curriculum-vitae',
@@ -6,10 +18,90 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./curriculum-vitae.component.css']
 })
 export class CurriculumVitaeComponent implements OnInit {
-
-  constructor() { }
-
+  
+  name=""
+  formCurriculum: FormGroup
+  form: FormGroup
+  formBuilder: any;
+  model: FormObject
+  data: any = {};
+  constructor() {
+  }
   ngOnInit(): void {
+  }
+
+  // validaciones -----------------------------------------------------
+  private buildForm(): void {
+    this.formCurriculum = this.formBuilder.group({
+      apellidoM: ['', [Validators.required]],
+      apellidoP: ['', [Validators.required]],
+      nombres: ['', [Validators.required]],
+      ci: ['', [Validators.required]],
+      fechaNac: ['', [Validators.required]],
+      lugar: ['', [Validators.required]],
+      emision: ['', [Validators.required]],
+      pais: ['', [Validators.required]],
+      genero: ['', [Validators.required]],
+      estadoCivil: ['', [Validators.required]],
+
+
+
+      fecha: ['', [Validators.required]],
+    });
+  }
+
+  save(event: Event): void {
+    event.preventDefault();
+    if (this.formCurriculum.valid) {
+      const value = this.formCurriculum.value;
+    } else {
+      this.formCurriculum.markAllAsTouched();
+    }
+  }
+
+  get eventoForm() {
+    return this.formCurriculum.get('evento');
+  }
+  get eventoFormIsValid() {
+    return this.eventoForm.touched && this.eventoForm.valid;
+  }
+  get eventoFormIsInvalid() {
+    return this.eventoForm.touched && this.eventoForm.invalid;
+  }
+
+  get fechaForm() {
+    return this.formCurriculum.get('fecha');
+  }
+
+  get fechaIsValid() {
+    return this.fechaForm.touched && this.fechaForm.valid;
+  }
+
+  get fechaIsInvalid() {
+    return this.fechaForm.touched && this.fechaForm.invalid;
+  }
+
+  formValido() {
+    if (this.formCurriculum.valid) {
+    } else {
+      this.formCurriculum.markAllAsTouched();
+      tata.error('Error', 'Formulario invalido');
+    }
+  }
+
+  guardar( form: NgForm ){
+    if(form.invalid){
+      Object.values(form.controls).forEach(
+        control =>{
+          control.markAllAsTouched();
+        }
+      );
+    }
+    console.log(form.value);
+  }
+
+  resetForm(): void {
+    
   }
 
 }
