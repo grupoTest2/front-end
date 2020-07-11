e6aabf5ab2329a26c76480322a636251a095ea95
kevinneyder <kevinneyderbutronm@gmail.com>
2020-07-10 23:48:29 -0400

    Estudios y cursos

--- src/app/components/curriculum-vitae/estudios-cursos/estudios-cursos.component.ts
+++ src/app/components/curriculum-vitae/estudios-cursos/estudios-cursos.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+declare var tata: any;
+declare var $: any;
 @Component({
   selector: 'app-estudios-cursos',
   templateUrl: './estudios-cursos.component.html',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstudiosCursosComponent implements OnInit {
 
+  form: NgForm = new NgForm([],[]);
   constructor() { }
-
   ngOnInit(): void {
+  }
+
+  reset(){
+    this.form.resetForm();
+  }
+  guardar( form: NgForm){
+    this.form = form;
+    console.log("metodoooooooooooooooo")
+    if(form.invalid){
+      Object.values(form.controls).forEach(
+        control =>{
+          control.markAllAsTouched();
+        }
+      );
+      tata.error('Error','Llene todos los campos por favor');
+    }else{
+
+      $('#modalEstudiosCursosTomados').modal('hide');
+      tata.success('Exitoso','Se guardaron sus datos');
+    console.log(form.value)
+    
+    }
   }
 
 }
