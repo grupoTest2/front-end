33c44e532bef45a599f3e10c8606554f5402d5ec
kevinneyder <kevinneyderbutronm@gmail.com>
2020-07-19 12:12:48 -0400

    filtro postulantes

---
+++ src/app/pipes/postulante/filtro-cod-sis.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FiltroCodSisPipe } from './filtro-cod-sis.pipe';
+
+describe('FiltroCodSisPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FiltroCodSisPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
